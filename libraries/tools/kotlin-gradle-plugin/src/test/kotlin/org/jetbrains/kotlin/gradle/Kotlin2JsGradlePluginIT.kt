aca75fc3051aab4189229bd243bb2a1ba271e2b6
Zoltan Nagy <abesto@abesto.net>
2014-11-01 20:02:08 +0100

    Reformat code in Kotlin2JsGradlePluginIT.kt

--- libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/Kotlin2JsGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/Kotlin2JsGradlePluginIT.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 import org.jetbrains.kotlin.gradle.BaseGradleIT.Project
 
 
-class Kotlin2JsGradlePluginIT: BaseGradleIT() {
+class Kotlin2JsGradlePluginIT : BaseGradleIT() {
     val project = Project("kotlin2JsProject", "1.6")
 
     Test fun testBuildAndClean() {
@@ -13,14 +13,14 @@ class Kotlin2JsGradlePluginIT: BaseGradleIT() {
             assertReportExists()
 
             assertContains(":libraryProject:jarSources\n",
-                           ":mainProject:copyKotlinSourcesForSourceMap\n", ":mainProject:rewritePathsInSourceMap\n",
-                           ":mainProject:compileKotlin2Js\n",    ":mainProject:copyKotlinJs\n",
-                           ":libraryProject:compileKotlin2Js\n", ":libraryProject:copyKotlinJs\n")
+                    ":mainProject:copyKotlinSourcesForSourceMap\n", ":mainProject:rewritePathsInSourceMap\n",
+                    ":mainProject:compileKotlin2Js\n", ":mainProject:copyKotlinJs\n",
+                    ":libraryProject:compileKotlin2Js\n", ":libraryProject:copyKotlinJs\n")
 
-            listOf("mainProject/web/js/app.js",                  "mainProject/web/js/lib/kotlin.js",
-                   "libraryProject/build/kotlin2js/main/app.js", "libraryProject/build/kotlin2js/main/kotlin.js",
-                   "mainProject/web/js/app.js.map", "mainProject/web/js/example/main.kt"
-            ).forEach{assertFileExists(it)}
+            listOf("mainProject/web/js/app.js", "mainProject/web/js/lib/kotlin.js",
+                    "libraryProject/build/kotlin2js/main/app.js", "libraryProject/build/kotlin2js/main/kotlin.js",
+                    "mainProject/web/js/app.js.map", "mainProject/web/js/example/main.kt"
+            ).forEach { assertFileExists(it) }
 
             assertFileContains("libraryProject/build/kotlin2js/main/app.js", "Counter: Kotlin.createClass")
             // Should be updated to `new _.example.library.Counter` once namespaced imports from libraryFiles are implemented
@@ -31,14 +31,14 @@ class Kotlin2JsGradlePluginIT: BaseGradleIT() {
         project.build("build") {
             assertSuccessful()
             assertContains(":mainProject:compileKotlin2Js UP-TO-DATE", ":libraryProject:compileTestKotlin2Js UP-TO-DATE",
-                           ":mainProject:copyKotlinSourcesForSourceMap UP-TO-DATE")
+                    ":mainProject:copyKotlinSourcesForSourceMap UP-TO-DATE")
         }
 
         project.build("clean") {
             assertSuccessful()
             assertReportExists()
             assertContains(":mainProject:cleanCompileKotlin2Js\n", ":mainProject:cleanCopyKotlinJs\n",
-                           ":mainProject:cleanCopyKotlinSourcesForSourceMap\n")
+                    ":mainProject:cleanCopyKotlinSourcesForSourceMap\n")
             assertNoSuchFile("mainProject/web/js/app.js")
             assertNoSuchFile("mainProject/web/js/lib/kotlin.js")
 
